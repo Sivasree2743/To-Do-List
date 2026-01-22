c6d20a2ecbf166d3bd7e33dddf39ccb0d896ccce
sivasree <sivasree978@gmail.com>
2026-01-22 11:26:14 +0530

    initial commit

---
+++ src/app/components/to-do/to-do.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+
+@Component({
+  selector: 'app-to-do',
+  standalone: true,
+  imports: [CommonModule,FormsModule],
+  templateUrl: './to-do.component.html',
+  styleUrl: './to-do.component.css'
+})
+export class ToDoComponent {
+
+  task="";
+  tasks=[];
+
+  addTask(){
+    if(this.task.trim()!==""){
+      this.tasks.push({
+        name:this.task,
+        completed:false
+      })
+    }
+    this.task=''
+  }
+
+  deleteTask(idx){
+    this.tasks.splice(idx,1)
+  }
+
+  toggleTask(task){
+    task.completed=!task.completed;
+  }
+
+}
